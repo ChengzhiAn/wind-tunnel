708c982c95fd9f81c2feea67ead6f8b403b25de3
ChengzhiAn <951761052@qq.com>
2026-04-22 14:53:40 +0800

    更新：添加自定义图标、启动屏幕、状态栏适配

--- android/app/src/main/java/com/lenghu/windtunnel/MainActivity.java
+++ android/app/src/main/java/com/lenghu/windtunnel/MainActivity.java
@@ -1,5 +1,27 @@
 package com.lenghu.windtunnel;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // 设置状态栏颜色为应用顶部渐变的起始色，让过渡更协调
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(android.graphics.Color.parseColor("#1a2e3a"));
+        }
+        
+        // 设置状态栏图标为浅色（深色背景）
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decorView = getWindow().getDecorView();
+            int flags = decorView.getSystemUiVisibility();
+            decorView.setSystemUiVisibility(flags & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+}
